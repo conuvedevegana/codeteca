92eecf8143f9aff180e19b78ddfd3d87de646c01
conuvedevegana <cardelsil@gmail.com>
2024-02-12 15:37:25 +0100

    created files architecture

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codeteca</title>
+</head>
+<body>
+    <h1>Bienvenida a la Codeteca :D</h1>
+</body>
+</html>
