c094666626f34926796cd51af10a23d2cd4e0113
conuvedevegana <cardelsil@gmail.com>
2024-02-15 17:16:51 +0100

    created index structure html

--- index.php
+++ index.php
@@ -14,7 +14,23 @@
     </nav>
 </header>
 <body>
-    <h1>Bienvenida a la Codeteca :D</h1>
+    <img class="banner" alt="" src=""></img>
+    <section class="booksPhotos">
+        <ul class=booksTop>
+            <li class="bookPhoto"><img src="resources\img\100_mujeres_que_cambiaron.jpg" alt="Portada de 100 Mujeres que cambiaron el mundo"></li>
+            <li class="bookPhoto"><img src="./resources/img/asesinato_en_el_orient.jpg" alt="Portada de Asesinato en el Orient Express"></li>
+            <li class="bookPhoto"><img src="resources\img\cuando_dios_era_mujer.jpg" alt="Portada de Cuando Dios era mujer"></li>
+            <li class="bookPhoto"><img src="resources\img\vozdevieja.jpg" alt="Portada de Vozdevieja"></li>
+            <li class="bookPhoto"><img src="resources\img\respondona.jpg" alt="Portada de Respondona"></li>
+        </ul>
+        <ul class=booksBottom>
+            <li class="bookPhoto"><img src="resources\img\mujeres_raza_clase.jpg" alt="Portada de Mujeres raza clase"></li>
+            <li class="bookPhoto"><img src="resources\img\ghostgirl.jpg" alt="Portada de Ghostgirl"></li>
+            <li class="bookPhoto"><img src="resources\img\mirada_inquieta.jpg" alt="Portada de La mirada inquieta: cómo disfrutar del arte con tus propios ojos"></li>
+            <li class="bookPhoto"><img src="resources\img\tecnologia_ciencia.jpg" alt="Portada de Tecnologia,Ciencia y Naturaleza"></li>
+            <li class="bookPhoto"><img src="resources\img\todo_sobre_el_amor.jpg" alt="Portada de Todo sobre el amor"></li>
+        </ul>
+    </section>
 </body>
 <footer>
     <p>Todos los derechos reservados</p>
