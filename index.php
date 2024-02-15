e7dae9c4b2fd831bce8b12a83920bf91f1816217
GabriiCoder <150010585+GabriiCoder@users.noreply.github.com>
2024-02-15 15:37:01 +0100

    created the footer

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codeteca</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <h1>Bienvenida a la Codeteca :D</h1>
 </body>
+<footer>
+    <p>Todos los derechos reservados</p>
+    <section class="socialMedia">
+        <ul>
+        <li><a href="https://www.facebook.com" class="fa-brands fa-facebook"><i aria-label="Enlace a facebook"></i></a></li>
+        <li><a href="https://www.instagram.com" class="fa-brands fa-instagram"><i aria-label="Enlace a instagram"></i></a></li>
+        <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
+        <ul>
+    </section>
+</footer>
 </html>
