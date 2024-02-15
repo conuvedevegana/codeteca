eaceb3e8ba63807393d1c27f0c7ad5c1946afb9a
conuvedevegana <cardelsil@gmail.com>
2024-02-15 15:01:14 +0100

    created header

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codeteca</title>
 </head>
+<header>
+    <nav class="headerBar">
+        <img class="logo" alt="logo de Codeteca" href=""></img>
+        <input id="searchBar" type="text" name="searchBar" placeholder="Escribe el título o autora"></input>
+        <button id="loginButton">Inicia sesión</button>
+    </nav>
+</header>
 <body>
     <h1>Bienvenida a la Codeteca :D</h1>
 </body>
