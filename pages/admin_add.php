47fcdde5466a7146e0fd3e13e44ea00a5d03e273
Carolina Chagas <thamelo.win@gmail.com>
2024-02-16 12:46:18 +0100

    fixed Add and Edit

--- pages/admin_add.php
+++ pages/admin_add.php
@@ -6,42 +6,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codeteca</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="resources\css\admin_add.css">
 </head>
 
 <body>
-    <main class="main">
-        <section class="Add-book">
+    <header>
+        <nav class="headerBar">
+            <img class="logo" alt="logo de Codeteca" href="index.php"></img>
+            <input id="searchBar" type="text" name="searchBar" placeholder="Escribe el título o autora"></input>
+            <button id="addBookButton" href="pages\admin_add.php">Añadir libro</button>
+            <button id="loginButton" href="pages\admin_login.php">Inicia sesión</button>
+        </nav>
+    </header>
+
+    <main>
+        <section class="addBook">
             <h2>Agregar libro</h2>
             <label>Titulo</label>
             <input id="searchBar" type="text" name="título" placeholder="Escribe el título"></input>
-            <label>Autor</label>
-            <input id="searchBar" type="text" name="Autor" placeholder="Escribe el nombre del autor"></input>
+            <label>Autora</label>
+            <input id="searchBar" type="text" name="Autora" placeholder="Escribe el nombre de la autora"></input>
             <label>ISBN</label>
             <input id="searchBar" type="text" name="ISBN" placeholder="ISBN"></input>
-            <section class="book-description">
+            <section class="bookDescription">
                 <label>Descripcion: </label>
                 <textarea name="Description">
                 </textarea>
             </section>
-            <section class="book-cover">
-                <input id="Imagen" type="file" name="book-cover" alt="Imagen"></input>
+            <section class="uploadCover">
+                <label class="bookCover"> Portada del libro: </label>
+                <input type="file" name="bookCover" accept="image/*">
             </section>
-            <section class="actions">
-                <button>Salvar</button>
-                <button>Cancelar</button>
-                <button>Publicar</button>
+            <section class="editingButtons">
+                <button id=saveButton>Guardar</button>
+                <button id=cancelButton>Cancelar</button>
             </section>
     </main>
+
+    <footer>
+        <p>Todos los derechos reservados</p>
+        <section class="socialMedia">
+            <ul>
+                <li><a href="https://www.facebook.com" class="fa-brands fa-facebook"><i aria-label="Enlace a facebook"></i></a></li>
+                <li><a href="https://www.instagram.com" class="fa-brands fa-instagram"><i aria-label="Enlace a instagram"></i></a></li>
+                <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
+            </ul>
+        </section>
+    </footer>
 </body>
-<footer>
-    <p>Todos los derechos reservados</p>
-    <section class="socialMedia">
-        <ul>
-            <li><a href="https://www.facebook.com" class="fa-brands fa-facebook"><i aria-label="Enlace a facebook"></i></a></li>
-            <li><a href="https://www.instagram.com" class="fa-brands fa-instagram"><i aria-label="Enlace a instagram"></i></a></li>
-            <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
-        </ul>
-    </section>
-</footer>
 
 </html>
--- pages/admin_edit.php
+++ pages/admin_edit.php
@@ -6,45 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codeteca</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="resources\css\admin_edit.css">
 </head>
 
 <body>
-    <main class="main">
-        <section class="book-details">
-            <h2>MUJERES QUE CORREN CON LOS LOBOS</h2>
-            <div>
-                <button>Ver/Editar Item</button>
-                <button>Deletar Item?</button>
-            </div>
-            <section class="book-description">
+    <header>
+        <nav class="headerBar">
+            <img class="logo" alt="logo de Codeteca" href="index.php"></img>
+            <input id="searchBar" type="text" name="searchBar" placeholder="Escribe el título o autora"></input>
+            <button id="addBookButton" href="pages\admin_add.php">Añadir libro</button>
+            <button id="loginButton" href="pages\admin_login.php">Inicia sesión</button>
+        </nav>
+    </header>
+
+    <main>
+        <section class="bookDetails">
+            <h2 class="bookTitle"></h2>
+            <h3 class="bookAuthor"></h3>
+            <section class="bookDescription">
                 <label>Descripcion: </label>
                 <textarea name="Description">
                 </textarea>
             </section>
         </section>
-        <section class="book-description">
-
-            <p></p>
+        <section class="editingButtons">
+            <button id=saveButton>Guardar</button>
+            <button id=cancelButton>Cancelar</button>
         </section>
-        <section class="book-cover">
-            <img src="" alt="Book cover">
+        <section class="uploadCover">
+            <label class="bookCover"> Portada del libro: </label>
+            <input type="file" name="bookCover" accept="image/*">
         </section>
-        <section class="actions">
-            <button>Publicar</button>
-            <button>Cancelar</button>
-        </section>
-
     </main>
+    <footer>
+        <p>Todos los derechos reservados</p>
+        <section class="socialMedia">
+            <ul>
+                <li><a href="https://www.facebook.com" class="fa-brands fa-facebook"><i aria-label="Enlace a facebook"></i></a></li>
+                <li><a href="https://www.instagram.com" class="fa-brands fa-instagram"><i aria-label="Enlace a instagram"></i></a></li>
+                <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
+            </ul>
+        </section>
+    </footer>
 </body>
-<footer>
-    <p>Todos los derechos reservados</p>
-    <section class="socialMedia">
-        <ul>
-            <li><a href="https://www.facebook.com" class="fa-brands fa-facebook"><i aria-label="Enlace a facebook"></i></a></li>
-            <li><a href="https://www.instagram.com" class="fa-brands fa-instagram"><i aria-label="Enlace a instagram"></i></a></li>
-            <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
-        </ul>
-    </section>
-</footer>
 
 </html>
