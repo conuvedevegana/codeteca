5789f1e0549bd2a04c1793b0757649b775a7fae8
Carolina Chagas <thamelo.win@gmail.com>
2024-02-19 00:56:12 +0100

    admin add page

--- pages/admin_add.php
+++ pages/admin_add.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Codeteca</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="resources\css\admin_add.css">
+    <link rel="stylesheet" href="../resources/css/admin_add.css">
 </head>
 
 <body>
@@ -20,27 +20,41 @@
     </header>
 
     <main>
-        <section class="addBook">
-            <h2>Agregar libro</h2>
-            <label>Titulo</label>
-            <input id="searchBar" type="text" name="título" placeholder="Escribe el título"></input>
-            <label>Autora</label>
-            <input id="searchBar" type="text" name="Autora" placeholder="Escribe el nombre de la autora"></input>
-            <label>ISBN</label>
-            <input id="searchBar" type="text" name="ISBN" placeholder="ISBN"></input>
-            <section class="bookDescription">
-                <label>Descripcion: </label>
-                <textarea name="Description">
+        <aside>
+            <div class="bookCoverAdd">
+                <section class="uploadCover">
+                    <label class="bookCover"> Portada del libro: </label>
+                    <input type="file" name="bookCover" accept="image/*">
+                    </section>
+                    <section class="uploadCoverButtons">
+                    <button id=uploadCoverButton>Añadir foto</button>
+                    </section>
+
+                
+        </aside>
+        </div>
+        <div class="addDash">
+            <section class="addBook">
+                <h2>Agregar libro</h2>
+                <label>Titulo</label>
+                <input id="searchBar" type="text" name="título" placeholder="Escribe el título"></input>
+                <label>Autora</label>
+                <input id="searchBar" type="text" name="Autora" placeholder="Escribe el nombre de la autora"></input>
+                <label>Genero</label>
+                <input id="searchBar" type="text" name="Genero" placeholder="Genero"></input>
+                <label>ISBN</label>
+                <input id="searchBar" type="text" name="ISBN" placeholder="ISBN"></input>
+                <section class="bookDescription">
+                    <label>Descripcion: </label>
+                    <textarea name="Description">
                 </textarea>
-            </section>
-            <section class="uploadCover">
-                <label class="bookCover"> Portada del libro: </label>
-                <input type="file" name="bookCover" accept="image/*">
-            </section>
-            <section class="editingButtons">
-                <button id=saveButton>Guardar</button>
-                <button id=cancelButton>Cancelar</button>
-            </section>
+                </section>
+                <section class="editingButtons">
+                    <button id=saveButton>Guardar</button>
+                    <button id=cancelButton>Cancelar</button>
+                    <button id=publishButton>Publicar</button>
+                </section>
+        </div>
     </main>
 
     <footer>
