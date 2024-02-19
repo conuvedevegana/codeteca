8f221bdbb1783cc21225e0434afaa2b73e092d9c
GabriiCoder <150010585+GabriiCoder@users.noreply.github.com>
2024-02-19 12:10:14 +0100

    fixed index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-]$id]
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,18 +25,18 @@
         <img class="banner" alt="lomos de libros de diferentes colores en una estantería" src="resources\img\banner.jpg">
         <section class="booksPhotos">
             <ul class=booksTop>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\100_mujeres_que_cambiaron.jpg" alt="Portada de 100 Mujeres que cambiaron el mundo"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="./resources/img/asesinato_en_el_orient.jpg" alt="Portada de Asesinato en el Orient Express"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\cuando_dios_era_mujer.jpg" alt="Portada de Cuando Dios era mujer"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\vozdevieja.jpg" alt="Portada de Vozdevieja"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\respondona.jpg" alt="Portada de Respondona"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\100_mujeres_que_cambiaron.jpg" alt="Portada de 100 Mujeres que cambiaron el mundo"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="./resources/img/asesinato_en_el_orient.jpg" alt="Portada de Asesinato en el Orient Express"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\cuando_dios_era_mujer.jpg" alt="Portada de Cuando Dios era mujer"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\vozdevieja.jpg" alt="Portada de Vozdevieja"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\respondona.jpg" alt="Portada de Respondona"></a></li>
             </ul>
             <ul class=booksBottom>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\mujeres_raza_clase.jpg" alt="Portada de Mujeres raza clase"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\ghostgirl.jpg" alt="Portada de Ghostgirl"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\mirada_inquieta.jpg" alt="Portada de La mirada inquieta: cómo disfrutar del arte con tus propios ojos"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\tecnologia_ciencia.jpg" alt="Portada de Tecnologia,Ciencia y Naturaleza"></a></li>
-                <li class="bookPhoto"><a href="pages\book_info.php"><img src="resources\img\todo_sobre_el_amor.jpg" alt="Portada de Todo sobre el amor"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\mujeres_raza_clase.jpg" alt="Portada de Mujeres raza clase"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\ghostgirl.jpg" alt="Portada de Ghostgirl"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\mirada_inquieta.jpg" alt="Portada de La mirada inquieta: cómo disfrutar del arte con tus propios ojos"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\tecnologia_ciencia.jpg" alt="Portada de Tecnologia,Ciencia y Naturaleza"></a></li>
+                <li class="bookPhoto"><a href="./src/view/pages/book_info.php"><img src="resources\img\todo_sobre_el_amor.jpg" alt="Portada de Todo sobre el amor"></a></li>
             </ul>
         </section>
     </main>
@@ -49,7 +49,7 @@
                 <li><a href="https://www.twitter.com" class="fa-brands fa-twitter"><i aria-label="Enlace a X"></i></a></li>
             </ul>
         </section>
-        <h1>htmlspecialchars_decode</h1>
+        
     </footer>
 </body>
 
